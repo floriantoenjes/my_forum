74fbfb67b8809e0213da46cba5589dae46c75994
Florian Tönjes <skatepainter@yahoo.de>
2017-01-25 10:44:41 +0100

    Added a ToDo for a bug fix

--- src/main/java/com/floriantoenjes/forum/board/BoardController.java
+++ src/main/java/com/floriantoenjes/forum/board/BoardController.java
@@ -35,6 +35,7 @@ public class BoardController {
             page = 0;
         }
 
+        // ToDo: Paging introduced a bug. All topics are now added to view regardless to what topic they belong
         Page p = topicService.findAll(new PageRequest(page, 5));
         ArrayList<Integer> pages = new ArrayList<>();
         IntStream.range(0, p.getTotalPages()).forEach(pages::add);

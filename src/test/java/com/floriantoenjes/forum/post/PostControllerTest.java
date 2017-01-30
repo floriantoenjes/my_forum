e326a1034a286c19b703d36ab6a888ded1107cdc
Florian Tönjes <skatepainter@yahoo.de>
2017-01-30 13:56:14 +0100

    Implemented viewPostFormTest

--- src/test/java/com/floriantoenjes/forum/post/PostControllerTest.java
+++ src/test/java/com/floriantoenjes/forum/post/PostControllerTest.java
@@ -1,5 +1,7 @@
 package com.floriantoenjes.forum.post;
 
+import com.floriantoenjes.forum.user.Role;
+import com.floriantoenjes.forum.user.User;
 import com.floriantoenjes.forum.user.UserService;
 import org.junit.Before;
 import org.junit.Test;
@@ -7,6 +9,8 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -20,6 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 public class PostControllerTest {
     private MockMvc mockMvc;
+    private User user;
 
     @Mock
     PostService postService;
@@ -32,20 +37,27 @@ public class PostControllerTest {
 
     @Before
     public void setUp() throws Exception {
+        user = new User("test_user", "password", new Role("ROLE_USER"));
+        SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(user, null));
+
         mockMvc = MockMvcBuilders.standaloneSetup(postController).build();
     }
 
-
     @Test
     public void viewPostFormTest() throws Exception {
+        final String TEXT = "Test text.";
+        Post post = new Post(user, TEXT);
+        when(postService.findOne(1L)).thenReturn(post);
+
         mockMvc.perform(get("/posts/1"))
 
                 .andExpect(status().isOk())
-                .andExpect(view().name("post"));
+                .andExpect(view().name("post"))
+                .andExpect(model().attribute("post", post));
     }
 
     @Test
-    public void updatePost() throws Exception {
+    public void updatePostTest() throws Exception {
 
     }
 

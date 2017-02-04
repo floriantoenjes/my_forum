78632cba3dd181cccd2ce5c4586c73343f732cae
Florian Tönjes <skatepainter@yahoo.de>
2017-02-04 14:18:31 +0100

    Minor changes

--- src/main/java/com/floriantoenjes/forum/web/FlashMessage.java
+++ src/main/java/com/floriantoenjes/forum/web/FlashMessage.java
@@ -9,6 +9,22 @@ public class FlashMessage {
         this.status = status;
     }
 
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
     public enum Status {
         SUCCESS,
         FAILED

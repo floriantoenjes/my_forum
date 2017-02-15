0055d62bb49ba3f39207199accfacae9849be680
Florian Tönjes <skatepainter@yahoo.de>
2017-02-15 16:47:11 +0100

    Removed a todo

--- src/test/java/com/floriantoenjes/forum/post/PostControllerTest.java
+++ src/test/java/com/floriantoenjes/forum/post/PostControllerTest.java
@@ -70,7 +70,7 @@ public class PostControllerTest {
         Post post = createMockPost();
         topic.addPost(post);
         when(postService.findOne(1L)).thenReturn(post);
-        // ToDo: Fix the updatePost test
+
         mockMvc.perform(post("/posts/1")
                 .param("text", TEST_TEXT))
 

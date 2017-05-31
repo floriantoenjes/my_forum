f6916151b175c354d9a0db9d1596a43ff5e1603f
Florian Tönjes <skatepainter@yahoo.de>
2017-05-31 16:35:55 +0200

    User is being authenticated after registration now

--- src/main/java/com/floriantoenjes/forum/user/UserController.java
+++ src/main/java/com/floriantoenjes/forum/user/UserController.java
@@ -1,6 +1,9 @@
 package com.floriantoenjes.forum.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -63,6 +66,10 @@ public class UserController {
             return "redirect:/register";
         }
         userService.save(user);
+
+        Authentication auth = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
         return "redirect:/boards/";
     }
 }
